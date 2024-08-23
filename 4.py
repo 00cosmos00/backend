fb65bcb3fa32a5e51e0dca3f2ac386ef89e7d63d
00cosmos00 <156374483+00cosmos00@users.noreply.github.com>
2024-08-23 17:26:55 -0400

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,11 @@
+print("register")
+user=input("ingrese su nombre: ")
+contraseña=(input("ingrese su contraseña"))
+print("login")
+user1=input("ingrese su nombre: ")
+contraseña1=(input("ingrese su contraseña"))
+
+if user1==user and contraseña1==contraseña:
+    print("usuaio y contraseña correcto")
+else:
+    print("contraseña o usuario incorrecto")
